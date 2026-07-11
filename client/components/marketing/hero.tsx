904e8a808fb09cde06536b8b13baa9913b4973f4
Hem Patel <hempatel777@yahoo.com>
2026-07-11 22:35:06 +0530

    landing: change CTA from 'Start collecting reviews' to 'Get started'

--- client/components/marketing/hero.tsx
+++ client/components/marketing/hero.tsx
@@ -49,7 +49,7 @@ export function Hero() {
                   size="lg"
                   className="btn-3d magnetic-child group rounded-xl bg-primary px-8 py-4 text-sm font-bold text-primary-foreground squishy"
                 >
-                  Start collecting reviews
+                  Get started
                   <ArrowRight className="size-4 transition-transform group-hover:translate-x-1" />
                 </Button>
               </div>
